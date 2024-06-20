b5216baed085e169fe638422ea931e681c52ba9c
saltedmuse <eliizavetasereda@gmail.com>
2024-06-20 13:58:15 -0400

    buzzfeed

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // JavaScript goes here.
+let q2FavChoiceHospital = document.querySelector("#favoriteChoice");
+let image1=document.querySelector(".image1");
+let image2=document.querySelector(".image2");
+let image3=document.querySelector(".image3");
+let image4=document.querySelector(".image4");
+let button= document.querySelector("button");
+
+
+button.addEventListener("click", function () {
+    let Drink= document.querySelector("#favoriteDrink").value;
+    if (Drink=== "coffee"&& q2FavChoiceHospital=== "hospital") {
+        
+    }});
